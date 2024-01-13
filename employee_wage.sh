de9a26e3fa52f1366764ffc8b9a3bb5d2c296dfa
Vedant Singh <singhvedant1701@gmail.com>
2024-01-13 14:14:48 +0530

    Use case 5

--- employee_wage.sh
+++ employee_wage.sh
@@ -10,6 +10,7 @@ dailyWage=$((wagePerHour * fullDayHour))
 partTimeSalary=$((wagePerHour*partTimeHour))
 monthly_earning=$((dailyWage * workingdaysPerMonth))
 
+
 # # Use Case 1
 # if [ isPresent -eq $checkAttendance ]; then
 #     echo "Employee is present."
@@ -32,26 +33,31 @@ monthly_earning=$((dailyWage * workingdaysPerMonth))
 # echo "Monthly Earning for $workingdaysPerMonth is $(( monthly_earning ))"
 
 
-option="${1}" 
-case ${option} in 
-   -att) 
-        if [ isPresent -eq $checkAttendance ]; then
-            echo "Employee is present."
-        else
-            echo "Employee is absent"
-        fi
-        ;; 
-   -daily)
-        echo "Daily wage with per hour wage of $wagePerHour, for Working $fullDayHour Hours is $dailyWage."
-        ;; 
-   -parttime)
-        echo "Part time employee wage for $partTimeHour hours is : $((partTimeSalary))"
-        ;; 
-   -monthly)
-        echo "Monthly Earning for $workingdaysPerMonth is $(( monthly_earning ))"
-        ;; 
-   *)  
-        echo "`basename ${0}`:usage: [-att Attendance] | [-daily Daily-wage] | [-parttime Part-time-wage] | [-monthly Monthly-Earning]" 
-        exit 1 # Command to come out of the program with status 1
-        ;; 
-esac 
+# option="${1}" 
+# case ${option} in 
+#    -att) 
+#         if [ isPresent -eq $checkAttendance ]; then
+#             echo "Employee is present."
+#         else
+#             echo "Employee is absent"
+#         fi
+#         ;; 
+#    -daily)
+#         echo "Daily wage with per hour wage of $wagePerHour, for Working $fullDayHour Hours is $dailyWage."
+#         ;; 
+#    -parttime)
+#         echo "Part time employee wage for $partTimeHour hours is : $((partTimeSalary))"
+#         ;; 
+#    -monthly)
+#         echo "Monthly Earning for $workingdaysPerMonth is $(( monthly_earning ))"
+#         ;; 
+#    *)  
+#         echo "`basename ${0}`:usage: [-att Attendance] | [-daily Daily-wage] | [-parttime Part-time-wage] | [-monthly Monthly-Earning]" 
+#         exit 1 
+#         ;; 
+# esac 
+
+
+# Use Case 5
+monthly_earning=$((dailyWage * workingdaysPerMonth))
+echo "Monthly Earning for $workingdaysPerMonth is $(( monthly_earning ))"
