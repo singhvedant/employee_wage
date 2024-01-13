d5fe63ffda8c4e7392cdd1e233ed3e4b6018ca31
Vedant Singh <singhvedant1701@gmail.com>
2024-01-13 13:09:39 +0530

    Use case 4 switch case

--- employee_wage.sh
+++ employee_wage.sh
@@ -6,25 +6,52 @@ wagePerHour=20;
 fullDayHour=8;
 partTimeHour=8;
 workingdaysPerMonth=20;
-dailyWage=0
-
-# Use Case 1
-if [ isPresent -eq $checkAttendance ]; then
-    echo "Employee is present."
-else
-    echo "Employee is absent"
-fi
-
-# Use Case 2
 dailyWage=$((wagePerHour * fullDayHour))
-echo "Daily wage with per hour wage of $wagePerHour, for Working $fullDayHour Hours is $dailyWage."
-
-# Use Case 3
-echo "Part time employee wage for $partTimeHour hours is : $((wagePerHour*partTimeHour))"
-
-# Use Case 4
-
-
-# Use Case 5
+partTimeSalary=$((wagePerHour*partTimeHour))
 monthly_earning=$((dailyWage * workingdaysPerMonth))
-echo "Monthly Earning for $workingdaysPerMonth is $(( monthly_earning ))"
+
+# # Use Case 1
+# if [ isPresent -eq $checkAttendance ]; then
+#     echo "Employee is present."
+# else
+#     echo "Employee is absent"
+# fi
+
+# # Use Case 2
+# dailyWage=$((wagePerHour * fullDayHour))
+# echo "Daily wage with per hour wage of $wagePerHour, for Working $fullDayHour Hours is $dailyWage."
+
+# # Use Case 3
+# echo "Part time employee wage for $partTimeHour hours is : $((partTimeSalary))"
+
+# # Use Case 4
+
+
+# # Use Case 5
+# monthly_earning=$((dailyWage * workingdaysPerMonth))
+# echo "Monthly Earning for $workingdaysPerMonth is $(( monthly_earning ))"
+
+
+option="${1}" 
+case ${option} in 
+   -att) 
+        if [ isPresent -eq $checkAttendance ]; then
+            echo "Employee is present."
+        else
+            echo "Employee is absent"
+        fi
+        ;; 
+   -daily)
+        echo "Daily wage with per hour wage of $wagePerHour, for Working $fullDayHour Hours is $dailyWage."
+        ;; 
+   -parttime)
+        echo "Part time employee wage for $partTimeHour hours is : $((partTimeSalary))"
+        ;; 
+   -monthly)
+        echo "Monthly Earning for $workingdaysPerMonth is $(( monthly_earning ))"
+        ;; 
+   *)  
+        echo "`basename ${0}`:usage: [-att Attendance] | [-daily Daily-wage] | [-parttime Part-time-wage] | [-monthly Monthly-Earning]" 
+        exit 1 # Command to come out of the program with status 1
+        ;; 
+esac 
