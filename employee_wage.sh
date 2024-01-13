07ece508d06b0b6aa479c9007221facb0f3a421b
Vedant Singh <singhvedant1701@gmail.com>
2024-01-13 11:52:24 +0530

    Use_case_1 implemented

--- employee_wage.sh
+++ employee_wage.sh
@@ -1,1 +1,18 @@
 echo "Welcome to employee wage computation program"
+
+isPresent=1
+checkAttendance=$((RANDOM%2))
+
+if [ isPresent -eq $checkAttendance ]; then
+    echo "Employee is present."
+else
+    echo "Employee is absent"
+fi
+
+function calculate_daily_wage_emp() {
+    wagePerHour=20;
+    fullDayHour=8;
+    echo $((wagePerHour*fullDayHour));
+}
+
+calculate_daily_wage_emp
