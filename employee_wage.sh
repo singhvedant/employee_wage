ea9b18051b12e4c7f9dc66abe6531d0876f236d0
Vedant Singh <singhvedant1701@gmail.com>
2024-01-13 12:25:07 +0530

    Part time Use case 3 added

--- employee_wage.sh
+++ employee_wage.sh
@@ -4,11 +4,17 @@ isPresent=1
 checkAttendance=$((RANDOM%2))
 wagePerHour=20;
 fullDayHour=8;
+partTimeHour=8;
 
+# Use Case 1
 if [ isPresent -eq $checkAttendance ]; then
     echo "Employee is present."
 else
     echo "Employee is absent"
 fi
 
+# Use Case 2
 echo "Daily wage :\nWage_Per_Hour -> $wagePerHour\nWorking Hours -> $fullDayHour\nTotal Daily Wage = $(($wagePerHour * $fullDayHour))"
+
+# Use Case 3
+echo "Part time employee wage is : $((wagePerHour*partTimeHour))"
